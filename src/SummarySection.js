ee6b6007f60157b3ea6e4b09c39855cf3c3ab36d
Carlo Giustozzi <carlog1118@gmail.com>
2020-01-17 14:36:51 -0500

    finished refactor

---
+++ src/SummarySection.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Currency from './Currency';
+import Total from './Total';
+
+class SummarySection extends React.Component {
+    render() {
+        const summary = Object.keys(this.props.selected).map((feature, idx) => {
+            const featureHash = feature + '-' + idx;
+            const selectedOption = this.props.selected[feature];
+      
+            return (
+              <div className="summary__option" key={featureHash}>
+                <div className="summary__option__label">{feature} </div>
+                <div className="summary__option__value">{selectedOption.name}</div>
+                <div className="summary__option__cost">
+                  <Currency cost={selectedOption.cost} />
+                </div>
+              </div>
+            );
+        });
+        return (
+            <section className="main__summary">
+                <h2>Your cart</h2>
+                {summary}
+                <div className="summary__total">
+                <div className="summary__total__label">Total</div>
+                <div className="summary__total__value">
+                    <Total selected={this.props.selected} />
+                </div>
+                </div>
+            </section>
+        )
+    }
+}
+
+export default SummarySection;
---
+++ src/Total.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const USCurrencyFormat = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+});
+
+class Total extends React.Component{
+    render() {
+        const total = Object.keys(this.props.selected).reduce(
+            (acc, curr) => acc + this.props.selected[curr].cost,
+            0
+        );
+        return (
+            <div className="summary__total__value">
+                {USCurrencyFormat.format(total)}
+            </div>
+
+        )
+    }
+}
+
+export default Total;
